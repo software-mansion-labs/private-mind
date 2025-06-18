d866f5b3e424717f274b32232f135c195f337382
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-06-18 13:55:56 +0200

    feat: move import/export/delete to different places to match figma designs

--- components/chat-screen/ChatTitleWithMenu.tsx
+++ components/chat-screen/ChatTitleWithMenu.tsx
@@ -1,58 +1,25 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
-import { MenuView } from '@react-native-menu/menu';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Text, StyleSheet, View } from 'react-native';
 import { useTheme } from '../../context/ThemeContext';
 import { fontFamily, fontSizes } from '../../styles/fontFamily';
 
 interface Props {
   title: string;
   modelName: string;
-  onSelect: (action: string) => void;
 }
 
-const ChatTitleWithMenu = ({ title, modelName, onSelect }: Props) => {
+const ChatTitleWithMenu = ({ title, modelName }: Props) => {
   const { theme } = useTheme();
 
   return (
-    <MenuView
-      title={title}
-      onPressAction={({ nativeEvent }) => onSelect(nativeEvent.event)}
-      actions={[
-        {
-          id: 'import',
-          title: 'Import',
-        },
-        {
-          id: 'export',
-          title: 'Export',
-        },
-        {
-          id: 'delete',
-          title: 'Delete',
-          attributes: {
-            destructive: true,
-          },
-        },
-      ]}
-    >
-      <TouchableOpacity style={styles.titleContainer}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        >
-          <Text style={{ ...styles.title, color: theme.text.primary }}>
-            {title}
-          </Text>
-          <Ionicons name="chevron-forward" size={16} color="gray" />
-        </View>
-      </TouchableOpacity>
+    <View style={styles.titleContainer}>
+      <Text style={{ ...styles.title, color: theme.text.primary }}>
+        {title}
+      </Text>
       <Text style={{ ...styles.modelName, color: theme.text.defaultSecondary }}>
         {modelName}
       </Text>
-    </MenuView>
+    </View>
   );
 };
 
@@ -60,6 +27,7 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: fontSizes.md,
