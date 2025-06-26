8ae94700bfacc49319083f1caf33866c8efbf965
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-06-26 11:12:08 +0200

    fix: changes missing after rebase

--- styles/colors.ts
+++ styles/colors.ts
@@ -44,25 +44,4 @@ export const darkTheme = {
   },
 };
 
-// export const darkTheme = {
-//   bg: {
-//     softPrimary: '#020F3C',
-//     softSecondary: '#3D61D6',
-//     strongPrimary: '#FFFFFF',
-//     errorSecondary: '#8B2728',
-//     errorPrimary: '#DE595B',
-//   },
-//   text: {
-//     primary: '#FFFFFF',
-//     defaultSecondary: 'rgba(255, 255, 255, 0.8)',
-//     defaultTertiary: 'rgba(255, 255, 255, 0.6)',
-//     contrastTertiary: 'rgba(2, 15, 60, 0.6)',
-//     contrastPrimary: '#020F3C',
-//     error: '#E68485',
-//   },
-//   border: {
-//     soft: 'rgba(255, 255, 255, 0.2)',
-//   },
-// };
-
 export type ThemeType = typeof lightTheme;
