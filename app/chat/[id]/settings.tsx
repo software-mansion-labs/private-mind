4cc7dc50f9bbb1e5e78a43da46db084ce6993e59
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-07-15 13:09:24 +0200

    refactor: chat settings (#39)
    - created useChatSettings hook
    - splitted settings page into main and dummy component

--- app/chat/[id]/settings.tsx
+++ app/chat/[id]/settings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { RefObject, useMemo, useRef } from 'react';
 import {
   Text,
   StyleSheet,
@@ -9,71 +9,50 @@ import {
   Alert,
 } from 'react-native';
 import { router, useLocalSearchParams } from 'expo-router';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  getChatSettings,
-  setChatSettings,
-} from '../../../database/chatRepository';
+import { setChatSettings } from '../../../database/chatRepository';
 import { useSQLiteContext } from 'expo-sqlite';
 import PrimaryButton from '../../../components/PrimaryButton';
 import { useTheme } from '../../../context/ThemeContext';
 import { useModelStore } from '../../../store/modelStore';
-import ModelCard from '../../../components/model-hub/ModelCard';
 import { useChatStore } from '../../../store/chatStore';
-import TextFieldInput from '../../../components/TextFieldInput';
-import { fontFamily, fontSizes } from '../../../styles/fontFamily';
-import TextAreaField from '../../../components/TextAreaField';
 import ModalHeader from '../../../components/ModalHeader';
 import Toast from 'react-native-toast-message';
-import SecondaryButton from '../../../components/SecondaryButton';
 import { exportChatRoom } from '../../../database/exportImportRepository';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Theme } from '../../../styles/colors';
-import EntryButton from '../../../components/EntryButton';
-import InfoCircleIcon from '../../../assets/icons/info-circle.svg';
+import { useChatSettings } from '../../../hooks/useChatSettings';
+import ChatSettingsForm from '../../../components/settings/ChatSettingsForm';
 
 export default function ChatSettingsScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const chatId = Number(id) || null;
+  const isDefaultSettings = chatId === null;
+
   const scrollViewRef = useRef<ScrollView>(null);
   const insets = useSafeAreaInsets();
-
   const db = useSQLiteContext();
   const { theme } = useTheme();
-  const styles = useMemo(() => createStyles(theme), [theme]);
   const { getModelById } = useModelStore();
-  const { getChatById, renameChat, deleteChat } = useChatStore();
+  const { renameChat, deleteChat } = useChatStore();
 
-  const chat = getChatById(chatId as number);
+  const { settings, setSetting, chat } = useChatSettings(chatId);
   const model = chat?.modelId ? getModelById(chat?.modelId) : undefined;
 
-  const [chatTitle, setChatTitle] = useState(chat ? chat.title : ``);
-  const [systemPrompt, setSystemPrompt] = useState('');
-  const [contextWindow, setContextWindow] = useState('6');
-
-  useEffect(() => {
-    (async () => {
-      const settings = await getChatSettings(db, chatId);
-      setSystemPrompt(settings.systemPrompt);
-      setContextWindow(String(settings.contextWindow) || '6');
-    })();
-  }, [db, chatId]);
+  const styles = useMemo(() => createStyles(theme), [theme]);
 
   const handleSave = async () => {
     const newSettings = {
-      systemPrompt,
-      contextWindow: Number(contextWindow) || 6,
+      systemPrompt: settings.systemPrompt,
+      contextWindow: Number(settings.contextWindow) || 6,
     };
 
-    if (chatId === null) {
-      await AsyncStorage.setItem(
-        'default_chat_settings',
-        JSON.stringify(newSettings)
-      );
-    } else {
-      await setChatSettings(db, chatId, newSettings);
+    await setChatSettings(db, chatId, newSettings);
+
+    if (chatId !== null) {
       const newChatTitle =
-        chatTitle.length > 25 ? chatTitle.slice(0, 25) + '...' : chatTitle;
+        settings.title.length > 25
+          ? settings.title.slice(0, 25) + '...'
+          : settings.title;
       await renameChat(chatId, newChatTitle);
     }
 
@@ -106,11 +85,17 @@ export default function ChatSettingsScreen() {
     ]);
   };
 
-  const scrollToInput = async () => {
-    if (Platform.OS === 'ios') {
-      await new Promise((resolve) => setTimeout(resolve, 25));
-      scrollViewRef.current?.scrollTo({ y: 500, animated: true });
+  const handleExport = async () => {
+    if (!chatId) {
+      Alert.alert('Error', 'No chat to export.');
+      return;
     }
+
+    await exportChatRoom(db, chatId, settings.title);
+  };
+
+  const handleAppInfo = () => {
+    router.push('/modal/app-info');
   };
 
   return (
@@ -121,110 +106,16 @@ export default function ChatSettingsScreen() {
     >
       <View style={styles.container}>
         <ModalHeader title="Chat Settings" onClose={() => router.back()} />
-        <ScrollView
-          contentContainerStyle={styles.scrollViewContent}
-          showsVerticalScrollIndicator={false}
-          ref={scrollViewRef}
-        >
-          {chat && (
-            <View style={styles.textFieldSection}>
-              <Text style={styles.label}>Chat name</Text>
-              <TextFieldInput
-                value={chatTitle}
-                maxLength={25}
-                onChangeText={(val) => {
-                  setChatTitle(val);
-                }}
-              />
-            </View>
-          )}
-
-          {model && (
-            <View style={styles.textFieldSection}>
-              <Text style={styles.label}>Model in use</Text>
-              <Text style={styles.subLabel}>
-                Model selected during the creation of the chatroom. It cannot be
-                changed.
-              </Text>
-              <ModelCard model={model!} onPress={() => {}} />
-            </View>
-          )}
-
-          <View style={styles.textFieldSection}>
-            <Text style={styles.label}>Context Window</Text>
-            <Text style={styles.subLabel}>
-              Number of previously entered messages the model will have access
-              to.
-            </Text>
-            <TextFieldInput
-              value={contextWindow}
-              onChangeText={(val) => setContextWindow(val)}
-              placeholder="ex. 6"
-            />
-          </View>
-
-          <View style={styles.textFieldSection}>
-            <Text style={styles.label}>System Prompt</Text>
-            <Text style={styles.subLabel}>
-              Instruction defining the behavior of the model.
-            </Text>
-            <TextAreaField
-              value={systemPrompt}
-              onChangeText={setSystemPrompt}
-              placeholder="ex. Act as a IT support consultant and reply using only 1 sentence."
-              placeholderTextColor={theme.text.defaultTertiary}
-              onFocus={() => scrollToInput()}
-            />
-          </View>
-          {chat && (
-            <View style={styles.buttonSection}>
-              <SecondaryButton
-                text={'Export Chat'}
-                onPress={async () => {
-                  await exportChatRoom(db, chatId!, chatTitle);
-                }}
-              />
-              <SecondaryButton
-                text={'Delete Chat'}
-                style={{ borderColor: theme.bg.errorPrimary }}
-                textStyle={{ color: theme.text.error }}
-                onPress={handleDelete}
-              />
-              <EntryButton
-                text={'App Info'}
-                onPress={() => {
-                  router.push('/modal/app-info');
-                }}
-                style={{ justifyContent: 'center' }}
-                icon={
-                  <InfoCircleIcon
-                    width={20}
-                    height={20}
-                    style={{ color: theme.text.primary }}
-                  />
-                }
-              />
-            </View>
-          )}
-          {!chat && (
-            <View style={{ gap: 12 }}>
-              <EntryButton
-                text={'App Info'}
-                onPress={() => {
-                  router.push('/modal/app-info');
-                }}
-                style={{ justifyContent: 'center' }}
-                icon={
-                  <InfoCircleIcon
-                    width={20}
-                    height={20}
-                    style={{ color: theme.text.primary }}
-                  />
-                }
-              />
-            </View>
-          )}
-        </ScrollView>
+        <ChatSettingsForm
+          settings={settings}
+          setSetting={setSetting}
+          model={model}
+          isDefaultSettings={isDefaultSettings}
+          scrollViewRef={scrollViewRef as RefObject<ScrollView>}
+          onDelete={handleDelete}
+          onExport={handleExport}
+          onAppInfo={handleAppInfo}
+        />
         <PrimaryButton text="Save changes" onPress={handleSave} />
       </View>
     </KeyboardAvoidingView>
@@ -242,25 +133,5 @@ const createStyles = (theme: Theme) =>
       padding: 16,
       justifyContent: 'space-between',
       paddingBottom: Platform.OS === 'ios' ? 24 : 0,
-    },
-    scrollViewContent: {
-      gap: 24,
-      paddingBottom: 24,
-    },
-    textFieldSection: {
-      gap: 16,
-    },
-    buttonSection: {
-      gap: 12,
-    },
-    label: {
-      fontSize: fontSizes.md,
-      fontFamily: fontFamily.medium,
-      color: theme.text.primary,
-    },
-    subLabel: {
-      fontFamily: fontFamily.regular,
-      fontSize: fontSizes.sm,
-      color: theme.text.defaultSecondary,
     },
   });
